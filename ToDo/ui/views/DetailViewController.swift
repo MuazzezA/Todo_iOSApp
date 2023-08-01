fa51395fe0e0f6394958ba22dfcca072baac43e7
MuazzezA <ayd.muazzez@gmail.com>
2023-08-01 23:36:06 +0300

    Done: DetailViewController

--- ToDo/ui/views/DetailViewController.swift
+++ ToDo/ui/views/DetailViewController.swift
@@ -9,21 +9,30 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    var todoItem:Todo?
+    
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        idLabel.text = "Id: " +  String(todoItem?.id ?? -1)
+        titleTextField.text = todoItem?.title
+        descriptionTextField.text = todoItem?.description
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func updateButtonAct(_ sender: Any) {
+        if let title = titleTextField.text, let desc = descriptionTextField.text {
+            updateTodoInfo(new_titleText: title, new_descriptionText: desc)
+        }
     }
-    */
-
+    
+    func updateTodoInfo(new_titleText:String, new_descriptionText:String){
+        let alert = UIAlertController(title: "Güncellendi", message: "title : \(new_titleText) - description : \(new_descriptionText)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Tamam", comment: "Default action"), style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
