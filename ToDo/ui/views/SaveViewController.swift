55b14f179d0e314f20f614999afcce9bf6bf1e1b
MuazzezA <ayd.muazzez@gmail.com>
2023-08-01 23:35:54 +0300

    Done: SaveViewController

--- ToDo/ui/views/SaveViewController.swift
+++ ToDo/ui/views/SaveViewController.swift
@@ -9,21 +9,26 @@ import UIKit
 
 class SaveViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+            
+        
+       
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButtonAct(_ sender: Any) {
+        if let title = titleTextField.text, let desc = descTextField.text  {
+           saveTodoInfo(titleText: title, descriptionText: desc)
+        }
+        
     }
-    */
-
+    
+    func saveTodoInfo(titleText:String, descriptionText:String){
+        let alert = UIAlertController(title: "Kaydedildi", message: "title : \(titleText) - description : \(descriptionText)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Tamam", comment: "Default action"), style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
