53b750d449af6268c4e3339002a7b450fdfa068c
MuazzezA <ayd.muazzez@gmail.com>
2023-08-17 00:47:21 +0300

    connect to db in homeviewmodel

--- ToDo/ui/model/HomeViewModel.swift
+++ ToDo/ui/model/HomeViewModel.swift
@@ -14,12 +14,14 @@ class HomeViewModel {
     var todoList = BehaviorSubject<[Todo]>(value: [Todo]())
     
     init(){
-        
+        copyDB()
         todoList = todoRepo.todoList
     }
     
     func deleteTodoInfo(id:Int){
         todoRepo.deleteTodoInfo(id: id)
+        loadTodoData() // etkisi yok ??? 
+       
     }
     
     func searchTodoInfo(searchText:String?){
@@ -28,5 +30,26 @@ class HomeViewModel {
     
     func loadTodoData(){
         todoRepo.loadTodoData()
+    }
+    
+    func copyDB() {
+        let bundlePath = Bundle.main.path(forResource: "todos", ofType: ".sqlite")
+        let filePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        
+        let dbUrl = URL(fileURLWithPath: filePath).appendingPathComponent("todos.sqlite")
+        
+        let fm = FileManager.default
+        
+        if fm.fileExists(atPath: dbUrl.path()){
+            print("db already exist")
+        }else{
+            do{
+                try fm.copyItem(atPath: bundlePath!, toPath: dbUrl.path)
+            }catch{
+                print(error.localizedDescription)
+            }
+            
+        }
+        
     }
 }
